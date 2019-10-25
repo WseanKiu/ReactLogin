17435e74376e103952af2a8f6cb1402ded85c901
wseankiu <winstonseanqui@gmail.com>
2019-10-25 11:19:51 +0800

    ready for checking

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { SafeAreaView, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 
 import styles from './styles';
 
 const Wrapper = ({ children }) => {
     return (
-        <SafeAreaView style={{ flex: 1}}>
-            <View style={styles.container}>
-                {children}
-            </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View style={styles.container}>
+                    {children}
+                </View>
+            </TouchableWithoutFeedback>
         </SafeAreaView>
     )
 };
--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -8,6 +8,7 @@ import { Logo } from '../components/Logo';
 import { InputWithTitle, InputWithDropdown } from '../components/TextInput';
 import { SolidColorButton } from '../components/Button';
 import { CheckboxWithText } from '../components/Checkbox';
+import { validateEmail } from '../helpers/textValidations';
 
 import { rememberUserCredentials } from '../actions/account';
 
@@ -48,7 +49,6 @@ class LoginForm extends Component {
     }
 
     handleEmailInput = (value) => {
-        console.log(value);
         this.setState({ email: value });
         this.checkEmailField(value);
     }
@@ -65,17 +65,15 @@ class LoginForm extends Component {
     }
 
     handleCheckBox = () => {
-        const { email } = this.props;
         const { check } = this.state;
-        console.log(this.props, check);
         this.setState({ check: check ? false : true });
     }
 
     handleSignIn = () => {
-        const { email, password, check, emailError, passwordError } = this.state;
+        const { email, password, check } = this.state;
 
         if (!this.checkEmailField(email) && !this.checkPasswordField(password)) {
-            alert(`Login Succesful! email ${emailError}, pass ${passwordError}`);
+            alert('Login success!');
             check ? this.props.dispatch(rememberUserCredentials(email, password)) : null;
             this.setState({ email: "", password: "" })
         }
@@ -90,7 +88,7 @@ class LoginForm extends Component {
                 disableButton: true,
             });
             error = true;
-        } else if (!this.validateTextIfEmail(value)) {
+        } else if (!validateEmail(value)) {
             this.setState({ emailErrorMessage: INVALID_EMAIL_MESSAGE, emailError: true, disableButton: true });
             error = true;
         } else {
@@ -112,11 +110,6 @@ class LoginForm extends Component {
             this.setState({ passwordError: false, passwordErrorMessage: "", disableButton: false });
         }
         return error;
-    }
-
-    validateTextIfEmail = (value) => {
-        const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-        return expression.test(String(value).toLowerCase());
     }
 
     render() {
